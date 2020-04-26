4dde939a2bd8b453055d695d889e21c448a425f5
iamsamuel <sammyola122@gmail.com>
2020-04-26 06:27:41 +0100

    laravel setup with adminlte also with vue and vue router

---
+++ resources/assets/js/app.js
@@ -0,0 +1,39 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+let routes = [
+  { path: '/dashboard', component:require('./components/DashboardComponent.vue')},
+  { path: '/profile', component:require('./components/ProfileComponent.vue')},
+  { path: '/user', component:require('./components/UsersComponent.vue')}
+]
+
+
+const router = new VueRouter({
+  mode: 'history', 	
+  routes // short for `routes: routes`
+})
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+const app = new Vue({
+    el: '#app',
+    router
+});
---
+++ resources/assets/js/components/UsersComponent.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 mt-5">
+                
+              <h3>Users Table</h3> <button class="btn btn-success btn-sm wol" data-toggle="modal" data-target="#addNew">Add New  <i class="fas fa-user-plus fa fw"></i></button> 
+                <table class="table">
+                  <thead class="thead-dark">
+                    <tr>
+                      <th scope="col">ID</th>
+                      <th scope="col">First</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Password</th>
+                      <th scope="col">Role</th>
+                      <th scope="col">Bio</th>
+                      <th scope="col">Registered_At</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                      <td>2</td>
+                      <td>4</td>
+                      <td>7</td>
+                      <td>
+                        <a href="#">
+                          <i class="fa fa-edit btn btn-primary btn-xs"></i>
+                        </a>
+                        <a href="#">
+                           <i class="fa fa-trash btn btn-danger btn-xs"></i> 
+                        </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td>Larry</td>
+                      <td>the Bird</td>
+                      <td>@twitter</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <!-- Modal -->
+                <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="addNewLabel">Add User</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Create</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
